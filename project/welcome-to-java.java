dbfed2abc64e80c9bc9f6d5d60bc3835b9ad720e
Mayur Kumbhar <mayur.kumbhar@outlook.com>
2021-10-09 17:18:53 +0530

    update for merge conflicts

--- project/welcome-to-java.java
+++ project/welcome-to-java.java
@@ -1,5 +1,6 @@
 class Assignment{
 public static void main(String []a){
 	System.out.println("Welcome to DevOps Assignment");
+	//Adding this line from collaborator to demonstrate merge conflicts
 }
 }
